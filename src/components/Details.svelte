d2bafe202f19ad751d43cd706cb6a2249ec8b1ed
Simon Wiles <simonjwiles@gmail.com>
2025-04-07 10:23:10 -0700

    More details

--- src/components/Details.svelte
+++ src/components/Details.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { scale } from 'svelte/transition';
 	/** @import { Item } from '$components/Table.svelte' */
 
 	/**
@@ -12,14 +13,14 @@
 	let details = $state();
 </script>
 
-<section bind:this={details} {...props}>
+<section transition:scale bind:this={details} {...props}>
 	<header>
 		{item[4]}
 	</header>
 	<div>
 		<details>
 			<summary>Project Description</summary>
-			{@html item[21].replace(/(\\r)?\\n/g, '<br>')}
+			<p>{@html item[21].replace(/(\\r)?\\n/g, '<br>')}</p>
 		</details>
 		<hr />
 		<dl>
@@ -50,11 +51,44 @@
 		</dl>
 		<hr />
 		<p>
-			Representative: {item[32]}
-			{item[31]} ({item[30]}, {item[33]})<br />
+			Representative: <strong>{item[32]} {item[31]}</strong> ({item[30]}, {item[33]})<br />
 			URL: <a href={item[34]}>{item[34]}</a><br />
 			{item[35]}<br />
 			Tel: {item[36]}
+		</p>
+		<p>
+			{#if item[38]}
+				Senator: <strong>{item[38]} {item[37]}</strong> ({item[39]})<br />
+				URL: <a href={item[40]}>{item[40]}</a><br />
+				{item[41]}<br />
+				Tel: {item[42]}<br />
+				Contact Form: <a href={item[43]}>{item[43]}</a><br />
+				{#if item[45]}
+					Twitter: <a href={`https://twitter.com/${item[45]}`}>@{item[45]}</a><br />
+				{/if}
+				{#if item[46]}
+					Facebook: <a href={`https://www.facebook.com/${item[46]}`}>{item[46]}</a><br />
+				{/if}
+			{:else}
+				No Senator Data
+			{/if}
+		</p>
+		<p>
+			{#if item[48]}
+				Senator: <strong>{item[48]} {item[47]}</strong> ({item[49]})<br />
+				URL: <a href={item[50]}>{item[50]}</a><br />
+				{item[51]}<br />
+				Tel: {item[52]}<br />
+				Contact Form: <a href={item[53]}>{item[53]}</a><br />
+				{#if item[54]}
+					Twitter: <a href={`https://twitter.com/${item[54]}`}>@{item[54]}</a><br />
+				{/if}
+				{#if item[55]}
+					Facebook: <a href={`https://www.facebook.com/${item[55]}`}>{item[55]}</a><br />
+				{/if}
+			{:else}
+				No Senator Data
+			{/if}
 		</p>
 	</div>
 	<button onclick={() => close()}>Close</button>
@@ -82,6 +116,7 @@
 		justify-content: space-between;
 		max-width: 800px;
 		translate: -50% 0;
+		z-index: 3;
 	}
 
 	header {
@@ -89,13 +124,17 @@
 		font-size: 1.4rem;
 		background-color: var(--primary-color);
 		color: #fff;
-		padding: 0.5rem;
+		padding: 0.5rem 1rem;
 	}
 
 	summary {
 		font-weight: bold;
 		color: var(--primary-color);
 		font-size: 1.2rem;
+	}
+
+	details p {
+		margin: 1rem 1rem;
 	}
 
 	div {
@@ -111,14 +150,34 @@
 		columns: 2;
 	}
 
+	dt,
+	dd {
+		break-inside: avoid;
+	}
+
 	dt {
 		font-weight: bold;
 		color: var(--primary-color);
-		font-size: 1.2rem;
+		font-size: 1.1rem;
 	}
 
 	dd {
 		padding-left: 0.5rem;
 		margin-bottom: 0.5rem;
+	}
+
+	button {
+		background-color: var(--primary-color);
+		color: #fff;
+		padding: 0.5rem 1rem;
+		border: none;
+		border-top: 1px solid var(--primary-color);
+		cursor: pointer;
+
+		&:hover {
+			background-color: #fff;
+			border-radius: 0 0 5px 5px;
+			color: var(--primary-color);
+		}
 	}
 </style>
