d75d671bd19e9bbd830aef41b6a9ff3054991cdb
Simon Wiles <simonjwiles@gmail.com>
2025-04-10 10:17:43 -0700

    Update to "about" copy

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -12,17 +12,23 @@
 
 {#snippet content()}
 	<p>
-		This database includes National Endowment for the Humanities (NEH) grants that we understand
-		were canceled by DOGE as of Thursday, April 3rd. It does not include individual fellowships at
-		this point. Our understanding is that this is the majority of currently-funded NEH grants, and
-		some projects do not appear in this database. Please reach out if a cancelled grant has not been
-		included by emailing comms@ach.org.
+		This database includes National Endowment for the Humanities (NEH) grants that were canceled by
+		DOGE as of Tuesday, April 8th. It does not include individual fellowships. We understand that
+		around 40 projects remain funded, including some that have been reinstated. Please reach out if
+		a cancelled grant has not been included by emailing comms@ach.org, or if you see a grant on this
+		list that has been reinstated.
 	</p>
 
 	<p>
 		“Discipline” is challenging for many of these grants, and was extrapolated from the disciplinary
 		tagging system used in the NEH database, mapping to the discipline of major professional
 		associations in the US.
+	</p>
+
+	<p>
+		The funding numbers are the amount awarded by NEH and represent a maximum cap of money lost by
+		the project. Most had spent down some of their funds, but lost whatever remained to finish the
+		project.
 	</p>
 
 	<p>
